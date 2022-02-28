11a11b6e94e577f2ea16e9109f17dc7765d7414b
Oliver Myatt <ollie-myatt95@hotmail.co.uk>
2022-02-28 18:31:40 +0000

    CDK workshop - test the hit counter

---
+++ lambda/hitcounter.js
@@ -0,0 +1,30 @@
+const { DynamoDB, Lambda } = require("aws-sdk");
+
+exports.handler = async function (event) {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+
+  //create AWS SDK clients
+  const dynamo = new DynamoDB();
+  const lambda = new Lambda();
+
+  await dynamo
+    .updateItem({
+      TableName: process.env.HITS_TABLE_NAME,
+      Key: { path: { S: event.path } },
+      UpdateExpression: "ADD hits :incr",
+      ExpressionAttributeValues: { ":incr": { N: "1" } },
+    })
+    .promise();
+
+  // call downstream function and capture response
+  const response = await lambda
+    .invoke({
+      FunctionName: process.env.DOWNSTREAM_FUNCTION_NAME,
+      Payload: JSON.stringify(event),
+    })
+    .promise();
+
+  console.log("downstream response:", JSON.stringify(response, undefined, 2));
+
+  return JSON.parse(response.Payload);
+};
--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,8 +1,24 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
-import {Construct} from "constructs";
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import { HitCounter } from "./hitcounter";
 
-export class CdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CdkWorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
+    });
+
+    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
+      downstream: hello,
+    });
+
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: helloWithCounter.handler,
+    });
   }
 }
