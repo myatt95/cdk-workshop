55af7e702e33d2c0d958b2a31e149a7e8c43ac0d
Oliver Myatt <ollie-myatt95@hotmail.co.uk>
2022-02-28 19:00:18 +0000

    add tests for hitcounter

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -17,6 +17,7 @@ export class HitCounter extends Construct {
 
     const table = new dynamodb.Table(this, "Hits", {
       partitionKey: { name: "path", type: dynamodb.AttributeType.STRING },
+      encryption: dynamodb.TableEncryption.AWS_MANAGED,
     });
 
     this.table = table;
--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -19,3 +19,56 @@ test("DynamoDB Table Created", () => {
   const template = Template.fromStack(stack);
   template.resourceCountIs("AWS::DynamoDB::Table", 1);
 });
+
+test("Lambda has environment variables", () => {
+  const stack = new cdk.Stack();
+
+  // WHEN
+  new HitCounter(stack, "MyTestConstruct", {
+    downstream: new lambda.Function(stack, "TestFunction", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "hello.handler",
+      code: lambda.Code.fromAsset("lambda"),
+    }),
+  });
+
+  //THEN
+  const template = Template.fromStack(stack);
+  const envCapture = new Capture();
+
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Environment: envCapture,
+  });
+
+  expect(envCapture.asObject()).toEqual({
+    Variables: {
+      DOWNSTREAM_FUNCTION_NAME: {
+        Ref: "TestFunction22AD90FC",
+      },
+      HITS_TABLE_NAME: {
+        Ref: "MyTestConstructHits24A357F0",
+      },
+    },
+  });
+});
+
+test("DynamoDB table created with encryption", () => {
+  const stack = new cdk.Stack();
+
+  // WHEN
+  new HitCounter(stack, "MyTestConstruct", {
+    downstream: new lambda.Function(stack, "TestFunction", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "hello.handler",
+      code: lambda.Code.fromAsset("lambda"),
+    }),
+  });
+
+  // THEN
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::DynamoDB::Table", {
+    SSESpecification: {
+      SSEEnabled: true,
+    },
+  });
+});
